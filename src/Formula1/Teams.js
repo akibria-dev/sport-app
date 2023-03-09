203932e3b1fa1efd0c91ac8677a125ff635d3a5c
akibria12 <ahkibria@hotmail.co.uk>
2023-03-09 16:10:35 +0000

    feature added in formula1-teamstandings

--- src/Formula1/Teams.js
+++ src/Formula1/Teams.js
@@ -1,14 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./F1.css";
-class F1TeamNews extends Component {
-  constructor(props) {
-    super(props);
+// class F1TeamNews extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      f1News: [],
-    };
-  }
-  componentDidMount() {
+//     this.state = {
+//       f1News: [],
+//     };
+//   }
+function F1TeamNews() {
+  const [data, setData] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const callApi = () => {
     const options = {
       method: "GET",
       headers: {
@@ -21,27 +27,42 @@ class F1TeamNews extends Component {
       options
     )
       .then((response) => response.json())
-      .then((response) => this.setState({ f1News: response.response }))
+      .then((data) => setData(data.response))
       .catch((err) => console.error(err));
-  }
+  };
 
-  render() {
-    return (
-      <div className="f1-news-container">
-        {this.state.f1News.map((f1newsinfo) => (
-          <div className="f1sports-news-container">
-            <div className="f1title">
-              <div key={f1newsinfo.id}>
-                <p>Position:{f1newsinfo.position}</p>
-                <p>Team:{f1newsinfo.team.name}</p>
-                <p>Points:{f1newsinfo.points}</p>
-                <p>Season:{f1newsinfo.season}</p>
-              </div>
+  return (
+    <div className="f1-news-container">
+      <input
+        type="text"
+        placeholder="Select a year"
+        style={{ backgroundColor: "blue", fontWeight: "bold", color: "white" }}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+
+      <button
+        style={{ backgroundColor: "cyan", fontWeight: "bold" }}
+        onClick={callApi}
+      >
+        Results
+      </button>
+      <p></p>
+      <p style={{ fontWeight: "bolder", fontSize: "30px" }}>Team Standings</p>
+
+      {data.map((f1newsinfo) => (
+        <div className="f1sports-news-container">
+          <div className="f1title">
+            <div key={f1newsinfo.id}>
+              <p>Position:{f1newsinfo.position}</p>
+              <p>Team:{f1newsinfo.team.name}</p>
+              <p>Points:{f1newsinfo.points}</p>
+              <p>Season:{f1newsinfo.season}</p>
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
+        </div>
+      ))}
+    </div>
+  );
 }
 export default F1TeamNews;
